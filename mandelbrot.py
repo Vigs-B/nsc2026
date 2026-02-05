5ca06a9671499f59cf13f0255d3d1a8d5879d265
Vignesh Bhat <vigneshb824@gmail.com>
2026-02-05 14:55:49 +0100

    added the naive python file as well

---
+++ mandelbrot.py
@@ -0,0 +1,17 @@
+"""
+Mandelbrot Set Generator
+Author: [Your Name]
+Course: Numerical Scientific Computing 2026
+"""
+def f(x):
+"""
+Example function.
+Parameters---------
+x : float
+Input value
+Returns------
+float
+Output value
+"""
+# TODO: Implement the algorithm
+pass
